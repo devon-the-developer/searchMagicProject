2aecd9ff54c754cc48722700a32ecb3ab86a9ac0
Devon Lumsden <devonlumsden@mail.com>
2019-05-01 19:54:37 +1200

    add handleChange SearchBox

--- client/components/SearchBox.jsx
+++ client/components/SearchBox.jsx
@@ -3,10 +3,24 @@ import React from 'react'
 import {connect} from 'react-redux'
 
 class SearchBox extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            searchValue: ''
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            searchValue: event.target.value
+        })  
+    }
+
     render() {
         return (
             <div>
-                <input type="text" placeholder="Search Card Here..." />
+                <input type="text" name="search" placeholder="Search Card Here..." onChange={this.handleChange} />
             </div>
         )
     }
